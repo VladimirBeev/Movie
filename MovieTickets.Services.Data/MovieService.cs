ac447037521c14b627dd24c16e2eb1f4d3274f71
Vladimir Beev <vladimir.beev89@gmail.com>
2023-07-25 11:44:30 +0300

    Update Layout View

---
+++ MovieTickets.Services.Data/MovieService.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Data.SqlClient.Server;
+using Microsoft.EntityFrameworkCore;
+
+using MovieTickets.Data;
+using MovieTickets.Services.Data.Interfaces;
+using MovieTickets.Web.ViewModels.Movie;
+
+using System;
+using System.Globalization;
+
+namespace MovieTickets.Services.Data
+{
+	public class MovieService : IMovieService
+	{
+		private readonly MovieDbContext dbContext;
+
+		public MovieService(MovieDbContext dbContext)
+		{
+			this.dbContext = dbContext;
+		}
+
+		public async Task<IEnumerable<AllMoviesViewModel>> GetAllMoviesAsync()
+		{
+			return await dbContext.Movies
+				.Select(m => new AllMoviesViewModel()
+				{
+					Id = m.Id,
+					Title = m.Title,
+					Description = m.Description,
+					EndDate = m.EndDate,
+					StartDate = m.StartDate,
+					Cinema = m.Cinema.Name,
+					ImageUrl = m.ImageUrl,
+					Price = m.Price,
+					MovieCategory = m.MovieCategory.ToString(),
+					Producer = m.Producer.Name
+
+				}).ToListAsync();
+		}
+	}
+}
---
+++ MovieTickets/Controllers/MovieController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+using MovieTickets.Services.Data.Interfaces;
+
+namespace MovieTickets.Web.Controllers
+{
+	public class MovieController : Controller
+	{
+		private readonly IMovieService movieService;
+
+		public MovieController(IMovieService movieService)
+		{
+			this.movieService = movieService;
+		}
+
+		public async Task<IActionResult> AllMovies()
+		{
+			var data = await movieService.GetAllMoviesAsync();
+			return View(data);
+		}
+	}
+}
